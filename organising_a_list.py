0988e2dcc322f2da7f0919abec9d7ed4cc0d9040
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-05-18 19:28:57 +0600

    Add files via upload

---
+++ organising_a_list.py
@@ -0,0 +1,6 @@
+list = ["BMW", "Benz", "Audi", "Toyota", "Honda"]
+list.sort() # Sort the list in ascending order
+#sort change a list alphabetically permanently
+print(list) # Output: ['Audi', 'Benz', 'BMW', 'Honda', 'Toyota']
+list.sort(reverse=True) # Sort the list in descending order
+print(list)
