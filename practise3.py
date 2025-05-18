dfae78eb6952a60d91c2da955886d2a2a2d9c623
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-05-18 15:53:41 +0600

    Add files via upload

---
+++ practise3.py
@@ -0,0 +1,11 @@
+list = ["BMW", "Benz", "Audi", "Toyota", "Honda"]
+list.sort() # Sort the list in ascending order
+print(list) # Output: ['Audi', 'Benz', 'BMW', 'Honda', 'Toyota']
+list.sort(reverse=True) # Sort the list in descending order
+print(list) # Output: ['Toyota', 'Honda', 'BMW', 'Benz', 'Audi']
+print("Here is the car list:")
+print(list)
+print("\nHere is the car list ")
+print(list)
+print("\nHere is the car list ")
+print(sorted(list))
