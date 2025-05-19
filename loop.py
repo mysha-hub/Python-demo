e0d13e99d36bc19466053958bfe5e1f62fd6e1c1
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-05-19 13:33:01 +0600

    Add files via upload

--- loop.py
+++ loop.py
@@ -4,3 +4,6 @@ for magicians in magicians:
     print(f"I can't wait to see your next trick, {magicians.title()}.")
 print("Thank you, everyone. That was a great magic show!")
 # The for loop iterates over each element in the list 'magicians' and assigns it to the variable 'magicians'.
+for magicians in magicians:
+    print(f"{magicians.title()},that was a great trick!\n")
+    print(f"I can't wait to see your next trick, {magicians.title()}.")
