53b2bdfb92f7b8a89b941e27725d43b75a6aa94b
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-05-19 10:08:00 +0600

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,4 @@
+magicians = ["alice", "david", "carol"]
+for magicians in magicians:
+    print(f"{magicians.title()}, that was a great trick!")
+    print(f"I can't wait to see your next trick, {magicians.title()}.")
