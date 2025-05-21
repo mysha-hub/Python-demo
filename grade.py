b06ed7e205db214b8a87e88b9cc10c5d5d5fd463
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-05-21 07:37:06 +0600

    Add files via upload

--- grade.py
+++ grade.py
@@ -7,3 +7,20 @@ elif score >= 40 and score < 60:
     print("Grade C")
 else:
     print("Failed")
+
+a = 5 + 4j 
+b = 10 + 9j 
+c =  a + b
+print(c)
+print(type(c))
+
+
+score = int(input("Enter score:"))
+if 80 <= score <= 100:
+    print("Grade A")
+elif 60 <= score < 80:
+    print("Grade B")
+elif 40 <= score < 60:
+    print("Grade C")
+else:
+    print("Failed")
