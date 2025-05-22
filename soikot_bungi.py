5bee763016bd730a3ac07a643c9a377b8f3cabc7
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-05-22 09:20:43 +0600

    Add files via upload

---
+++ soikot_bungi.py
@@ -0,0 +1,5 @@
+pizza_list = ["Cheese", "Pepperoni", "Veggie", "Hawaiian", "BBQ Chicken"]
+for pizza_list in pizza_list:
+    print(f"I like {pizza_list} pizza.")
+print("I really love pizza!")
+# The code above is a simple program that iterates through a list of pizza types and prints a message for each type, followed by a final message expressing love for pizza.
