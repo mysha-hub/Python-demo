7e1c1f73115ace8d038a822815d5b4e52a4a56ee
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-05-22 19:42:40 +0600

    Add files via upload

---
+++ squareing_method.py
@@ -0,0 +1,5 @@
+squares = []
+for value in range(1, 11):
+    squares.append(value ** 2)
+    print(squares)
+# The above code creates a list of squares of numbers from 1 to 10 and prints the list after each addition.
