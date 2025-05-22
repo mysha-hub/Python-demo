863b8de709c2edac432872998b85b31d2974c23f
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-05-22 09:15:13 +0600

    Add files via upload

--- practise4.py
+++ practise4.py
@@ -1,2 +1,6 @@
-a = input("Enter:")
-print(type(a))
+lists = ["dog", "cat", "fish"]
+for list in lists:
+    print(f"{lists} are great pets.")
+print("All of these pets are great!")
+for list in range(0, 2):
+    print(f"{lists} are great pets.")
