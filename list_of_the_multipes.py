bf81c5aa8234f518feed233168a43e5946e92fa2
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-05-23 07:34:07 +0600

    Add files via upload

--- list_of_the_multipes.py
+++ list_of_the_multipes.py
@@ -5,3 +5,17 @@ print(multiples)
 
 cubes = [value ** 3 for value in range(1, 11)]
 print(cubes)
+
+
+digits = []
+for value in range(1, 21):
+    cube = value ** 3
+    digits.append(cube)
+    print(digits)
+# This code creates a list of cubes of numbers from 1 to 20
+
+
+
+
+cubes = [value ** 3 for value in range(1, 21)]
+print(cubes)
