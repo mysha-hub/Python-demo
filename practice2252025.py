93e239255e42221b61c80bb127870748322c96d0
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-05-23 06:08:13 +0600

    Add files via upload

---
+++ practice2252025.py
@@ -0,0 +1,10 @@
+digits = [value **1 for value in range(1, 100001)]
+# This code creates a list of numbers from 1 to 100000
+print(digits)
+minimum = min(digits)
+maximum = max(digits)
+print(minimum)
+print(maximum)
+# This code finds the minimum and maximum values in the list
+sumation = sum(digits)
+print(sumation)
