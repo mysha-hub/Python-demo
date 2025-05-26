0213d58165437261b3dcb8a48682c990f8afd143
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-05-26 07:12:01 +0600

    Add files via upload

--- if_statement.py
+++ if_statement.py
@@ -4,3 +4,11 @@ for car in cars:
         print(car.upper())
     else:
         print(car.title())
+
+
+
+# Check if a requested topping is not anchovies
+requested_topping = input("Enter a pizza topping: ")
+if requested_topping != 'anchovies':
+    print("Hold the anchovies!")
+# Check if a requested topping is not anchovies, using an if-else statement
