127a022960c59e3d5917aca644e97f8c4eec7214
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-05-27 09:34:53 +0600

    Add files via upload

---
+++ practise27052025.py
@@ -0,0 +1,49 @@
+car = "subaru"
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi')    
+
+#1
+
+car = 'toyota'
+print('Is car == "toyota"? I predict True.')
+print(car == 'toyota')
+#2
+
+car = 'toyota'
+print('Is car == "toyota"? I predict True.')
+print(car == 'honda')
+#3
+car = "Honda"
+print("Is car == 'Honda'? I predict True.")
+print(car == 'BMW')
+#4
+car = "BMW"
+print("Is car == 'BMW'? I predict True.")
+print(car == 'BMW')
+#5
+car = "Tesla"
+print("Is car == 'Tesla'? I predict True.")
+print(car == 'Tesla')
+#6
+car = "Ford"
+print("Is car == 'Ford'? I predict True.")
+print(car == 'BMW')
+#7
+car = "Mercedes"
+print("Is car == 'Mercedes'? I predict True.")
+print(car == 'Mercedes')
+#8
+car = "Audi"
+print("Is car == 'Audi'? I predict True.")
+print(car == 'Audi')
+#9
+car = "Audi"
+print("Is car == 'Audi'? I predict True.")
+print(car == 'Mercedes')
+#10
+car = "Nissan"
+print("Is car == 'Nissan'? I predict True.")
+print(car == 'Nissan')
