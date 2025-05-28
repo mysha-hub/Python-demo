15d6c84e39db4e03e66bc3de30afbbdaa07c5607
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-05-28 06:43:01 +0600

    Add files via upload

--- elif_statement.py
+++ elif_statement.py
@@ -10,3 +10,14 @@ else:
 print(f"Your ticket price is ${price}.")
 # This code determines the ticket price based on the age of the person.
 # It uses an if-elif-else statement to check the age and assign the appropriate price.
+
+
+extra_toppings = input("Do you want extra toppings? ")
+if 'mushrooms' in extra_toppings:
+    print("Adding mushrooms to your pizza.")
+elif 'extra cheese' in extra_toppings:
+    print("Adding extra cheese to your pizza.")
+elif 'pepperoni' in extra_toppings:
+    print("Adding pepperoni to your pizza.")
+else:
+    print("Sorry,we don't serve this topping.")
