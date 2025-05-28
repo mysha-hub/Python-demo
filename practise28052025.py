bcca17f9c2f7b793a64eb0c9dd4452939c16c216
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-05-28 10:49:21 +0600

    Update practise28052025.py

--- practise28052025.py
+++ practise28052025.py
@@ -9,3 +9,30 @@ if alien_color == "Yellow":
     print("Yahoo,you earn 10 point.")
 else:
     print("Oops")
+
+#exercise5-5
+alien_color = "Red"
+if alien_color == "Red":
+    print("You earned 10 points")
+elif alien_color == "Yellow":
+    print("You earned 15 ponts")
+elif alien_color == "Green":
+    print("You earned 5 points")
+else:
+    print("Ooops")
+
+
+#exercise 5-6
+age = int(input("Enter your age:"))
+if age <= 2:
+    print("The person is baby.")
+elif age <= 4 and age > 2:
+    print("The person is toddler.")
+elif age <= 13 and age > 4:
+    print("THe person is a kid.")
+elif age <= 20 and age > 13:
+    print("The person is a teenager.")
+elif age <= 65 and age > 20:
+    print("The person is a adult.")
+else:
+    print("The person is older.")
