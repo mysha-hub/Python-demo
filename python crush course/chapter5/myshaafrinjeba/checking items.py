54410b74a1478cf989f95e03145fe039387d38d3
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-05-28 19:48:16 +0600

    Create checking _items.py

---
+++ python crush course/chapter5/myshaafrinjeba/checking items.py
@@ -0,0 +1,4 @@
+requested_toppings = ['peparoni', 'extra cheese', 'mashrom']
+for requested_topping in requested_toppings:
+    print(f'We are putting{requested_topping} in our pizza.')
+print("\nWe are done making pizza.")
