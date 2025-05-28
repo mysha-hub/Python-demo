b522155689cc2e0afde2709aeaba0f9a4e06d832
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-05-28 20:11:24 +0600

    Update checking items.py

--- python crush course/chapter5/myshaafrinjeba/checking items.py
+++ python crush course/chapter5/myshaafrinjeba/checking items.py
@@ -2,3 +2,8 @@ requested_toppings = ['peparoni', 'extra cheese', 'mashrom']
 for requested_topping in requested_toppings:
     print(f'We are putting{requested_topping} in our pizza.')
 print("\nWe are done making pizza.")
+
+requested_toppings = ['mushroom', 'green onion', 'shrimp']
+for requested_topping in requested_toppings:
+    print(f'My favorite topping is {requested_topping}. ')
+print("\nThis are my favorite toppings.")
