3bb32978f7a3b2b6151365a4841d6c44917cf10c
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-05-28 15:35:24 +0600

    Update 2025.py

--- experiment28/05/2025.py
+++ experiment28/05/2025.py
@@ -1,1 +1,11 @@
 
+fruit_list = ['mango','lichi', 'banana']
+my_favorite_fruit = input("Enter your favorite fruit : ")
+if {my_favorite_fruit} in fruit_list :
+    print("My favorite fruit is mango.")
+elif {my_favorite_fruit} in fruit_list :
+    print("My favorite fruit is lichi")
+elif {my_favorite_fruit} in fruit_list :
+    print("My favorite fruit is banana.")
+else:
+    print("My favorite fruit is not present hero.")
