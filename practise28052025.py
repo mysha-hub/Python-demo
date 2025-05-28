9f7b46601e06aef368dd844fa54251223ef74b35
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-05-28 08:39:21 +0600

    Update practise28052025.py

--- practise28052025.py
+++ practise28052025.py
@@ -3,4 +3,9 @@ if alien_color == 'green':
     print("Yahoo,you earned 5 points.")
 else:
     print("oops!")
-
+#exercise5-3
+alien_color = "Yellow"
+if alien_color == "Yellow":
+    print("Yahoo,you earn 10 point.")
+else:
+    print("Oops")
