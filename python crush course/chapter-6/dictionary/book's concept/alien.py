5f87627570e55a9019a81aeb06a918bd8532fc0e
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-05-31 20:09:15 +0600

    Update alien.py

--- python crush course/chapter-6/dictionary/book's concept/alien.py
+++ python crush course/chapter-6/dictionary/book's concept/alien.py
@@ -14,3 +14,10 @@ print(f"The alien color is {alien_0['color']}.")
 alien_0['color'] = 'red'
 print(f"My favorite food is {alien_0['food']}.")
 #In this way to modify the values of dictonary.
+
+alien_0 = {'color': 'red', 'food': 'pizza'}
+print(alien_0)
+alien_0['x_position'] = 0
+alien_0['x_position'] = 25
+print(alien_0)
+#This method is used to add elements to a dictonary.
