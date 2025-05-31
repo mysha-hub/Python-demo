934132f923cdde57193a5afea60903997f8e88f0
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-05-31 12:09:33 +0600

    Update alien.py

--- python crush course/chapter-6/dictionary/book's concept/alien.py
+++ python crush course/chapter-6/dictionary/book's concept/alien.py
@@ -1,3 +1,8 @@
 alien_o = {'color':'green', 'points':5}
 print(alien_o['color'])
 print(alien_o['points']
+
+
+alien_0 = {'color': 'red', 'food': 'pizza'}
+print(f'The alien color is {alien_0['color']}.')
+print(f'My favorite food is {alien_0['food']}.')      
