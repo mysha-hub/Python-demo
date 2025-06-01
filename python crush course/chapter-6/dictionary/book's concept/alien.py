376372f751a1ee7d39250f9706508cd361746abe
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-06-01 08:07:41 +0600

    Update alien.py

--- python crush course/chapter-6/dictionary/book's concept/alien.py
+++ python crush course/chapter-6/dictionary/book's concept/alien.py
@@ -21,3 +21,21 @@ alien_0['x_position'] = 0
 alien_0['x_position'] = 25
 print(alien_0)
 #This method is used to add elements to a dictonary.
+
+
+alien_0 = {'x_position': 0, 'y_position': 25, 'speed': 'medium'}
+print(f"Alien's original position{alien_0['x_position']}")
+#Think about two point based number system.This system is actually working like the same.The alien is moving rightside.
+
+if alien_0['speed'] == 'slow':
+    x_increment = 1
+#This statement defines that if the alien's speed is slow then the position will change 1 unit everytime.
+elif alien_0['speed'] == 'modium':
+    x_increment = 2
+    #This statement defines that if the alien's speed is medium then the position will change 2 unit everytime.
+elif alien_0['speed'] == 'fast':
+    x_increment = 3
+#This statement defines that if the alien's speed is medium then the position will change 3 unit everytime.
+#This line below indicates the position counting method of the alien.
+alien_0['x_position'] = alien_0['x_position'] + x_increment
+print(f"The new position of alien is{alien_0['x_position']}.")
