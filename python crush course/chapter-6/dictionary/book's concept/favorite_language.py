2c4f3ad1e7b01e73aa5802ec9c9b83284de9dca0
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-06-02 12:03:25 +0600

    Update favorite_language.py

--- python crush course/chapter-6/dictionary/book's concept/favorite_language.py
+++ python crush course/chapter-6/dictionary/book's concept/favorite_language.py
@@ -1,3 +1,12 @@
 favorite_language = {'Jeba': 'python', 'Soikot': 'Java script', 'monamee': 'CSS'}
 language = favorite_language['Jeba'].title()
 print(f"Jeba's favorite language is {language}"
+
+alien_0 = {'color': 'green', 'speed': 'slow'}
+
+point_value = alien_0.get('points', 'No point value assigned.')
+print(point_value)
+
+favorites = {'food': 'pizza', 'ponts': 5}
+mama = favorites.get('cloths', 'No point value assigned.')
+print(mama)
