7ca574efae62d902f1da9fc7f76cfdaf05e3636e
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-06-06 09:08:58 +0600

    Create question no- 4.py

---
+++ python crush course/chapter-6/dictionary/self practise/question no- 4.py
@@ -0,0 +1,4 @@
+fav_social = {'jeba': 'telegram', 'monamee': 'facebook', 'barsha':'instagram', 'soikot': 'telegram', 'pial': 'whats app'}
+for name, social_media in fav_social.items():
+    print(f"{name.title()}'s favorite social media is {social_media.title()}.")
+
