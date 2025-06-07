7fb531d5603f706afada05d744fca9f01cc79dbc
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-06-07 21:40:32 +0600

    Create alien.py

---
+++ experiment28/05/alien.py
@@ -0,0 +1,11 @@
+aliens = []
+for alien_number in range (30):
+    new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
+    aliens.append(new_alien)
+
+for alien in aliens[:3]:
+    if alien['color'] == 'green':
+        alien['color'] = 'yellow'
+        alien['points'] = 10
+        alien['speed'] = 'medium'
+        print(alien)
