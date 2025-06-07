7c69b73a0a2fa53b79d56b10f62c54a3689a5d07
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-06-07 11:35:20 +0600

    Update alien_0.py

--- python crush course/chapter-6/dictionary/book's concept/alien_0.py
+++ python crush course/chapter-6/dictionary/book's concept/alien_0.py
@@ -4,3 +4,23 @@ alien_2 = {'color': 'red', 'points': 15}
 aliens = [alien_0, alien_1, alien_2]
 for alien in aliens:
     print(alien)
+
+
+
+
+# Make an empty list for storing aliens.
+alien = []
+
+#make 30 green aliens.
+for alien_number in range(30):
+    new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
+    alien.append(new_alien)
+    
+
+#show the first 5 aliens.
+for alien in alien[:5]:
+    print(alien)
+print("...")
+
+
+
