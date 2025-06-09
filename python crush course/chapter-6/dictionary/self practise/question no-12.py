efc3c003bb531b7407b05d9aaeb311629a20a2ca
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-06-09 12:31:04 +0600

    Update question no-12.py

--- python crush course/chapter-6/dictionary/self practise/question no-12.py
+++ python crush course/chapter-6/dictionary/self practise/question no-12.py
@@ -1,1 +1,9 @@
-
+alien_0 = {'color': 'yellow', 'points': 10, 'speed': 'slow'}
+alien_1 = {'color': 'green', 'points': 15, 'speed': 'medium'}
+my_dict = {}
+n = int(input("Enter the number of keys:"))
+for i in range(n):
+    key = input("Enter the key:")
+    value = input("Enter the value:")
+    my_dict[key] = value
+    print("alien_2:", my_dict)
