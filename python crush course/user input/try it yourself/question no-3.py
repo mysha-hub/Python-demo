683508028c4134fbad5d15f84e71a771a36ad839
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-06-10 13:42:09 +0600

    Create question no-3.py

---
+++ python crush course/user input/try it yourself/question no-3.py
@@ -0,0 +1,8 @@
+a = int(input("Enter a number:"))
+
+
+if a % 10 == 0:
+    print("The number is divisible by 10")
+else:
+    print("The number is not divisible by 10")
+# This code checks if a number is divisible by 10 and responds accordingly.
