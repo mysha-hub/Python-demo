83c1527d0f6e384f95c968304fa74fc52e69e3c2
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-06-10 13:33:44 +0600

    Create question no -2.py

---
+++ python crush course/user input/try it yourself/question no -2.py
@@ -0,0 +1,6 @@
+a = int(input("How many people are you there for dinner?"))
+if a > 8:
+    print("Please wait for a table.")
+else:
+    print("Your table is ready.")
+# This code checks the number of people for dinner and responds accordingly.
