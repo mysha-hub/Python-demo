73b6352373a8c6b65025b6dc4675620d88b0660e
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-06-11 07:18:23 +0600

    Update counting.py

--- python crush course/user input/books concept/counting.py
+++ python crush course/user input/books concept/counting.py
@@ -1,1 +1,10 @@
-
+current_number = 1
+while current_number <= 5:
+    print("Current number is:", current_number)
+    current_number += 1
+current_number = 1
+while current_number <= 100:
+    print(f"{current_number}. I LOVE python!")
+    current_number += 1
+#This code will print the numbers from 1 to 5 and then print "I LOVE python!" 100 times, each time with the current number in front of it.
+# The first loop counts from 1 to 5, printing the current number each time.
