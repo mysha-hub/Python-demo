2aaf1f8f0c1f339484ad4aeb0f369b0903773c2c
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-06-11 18:19:27 +0600

    Update parrot.py

--- python crush course/user input/books concept/parrot.py
+++ python crush course/user input/books concept/parrot.py
@@ -5,3 +5,17 @@ message = ""
 while message != 'quit':
     message = input(prompt)
     print(message)
+
+
+
+
+prompt = "\nTell me something,I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program."
+active = True
+while active:
+    message = input(prompt)
+    if message == 'quit':
+        active = False
+    else:
+        # Print the message back to the user
+        print(message)
