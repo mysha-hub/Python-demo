1c6aed0d2da58a2665ab333bc0f7890d8cbb9bc3
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-06-11 21:29:40 +0600

    Update cities.py

--- python crush course/user input/books concept/cities.py
+++ python crush course/user input/books concept/cities.py
@@ -1,1 +1,9 @@
 
+prompt = "type a city name:"
+prompt += "we will reeturn you the value untill you type 'exit'."
+while True:
+    city = input(prompt)
+    if city == 'exit':
+        break
+    else:
+        print(f"I would like to vist {city.title()}")
