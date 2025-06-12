514eef4fe110330c1dee22914c210f07bc59b4c4
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-06-12 20:02:23 +0600

    Update counting.py

--- python crush course/user input/books concept/counting.py
+++ python crush course/user input/books concept/counting.py
@@ -8,3 +8,13 @@ while current_number <= 100:
     current_number += 1
 #This code will print the numbers from 1 to 5 and then print "I LOVE python!" 100 times, each time with the current number in front of it.
 # The first loop counts from 1 to 5, printing the current number each time.
+
+
+#counting odd numbers 1 to 100
+counting_number = 1
+while counting_number < 100:
+    counting_number +=1
+    if counting_number % 2 == 0:
+        continue
+    print(counting_number)
+# This code will print all odd numbers from 1 to 100
